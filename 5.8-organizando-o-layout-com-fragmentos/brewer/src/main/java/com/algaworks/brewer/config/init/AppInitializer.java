b648dfd56993bb1086ede20ae8213a75c257168f
Normandes Junior <normandesjr@gmail.com>
2016-06-21 10:17:53 -0300

    Projeto do curso

--- 5.8-organizando-o-layout-com-fragmentos/brewer/src/main/java/com/algaworks/brewer/config/init/AppInitializer.java
+++ 5.8-organizando-o-layout-com-fragmentos/brewer/src/main/java/com/algaworks/brewer/config/init/AppInitializer.java
@@ -1,5 +1,8 @@
 package com.algaworks.brewer.config.init;
 
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import com.algaworks.brewer.config.WebConfig;
@@ -19,6 +22,15 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
 	@Override
 	protected String[] getServletMappings() {
 		return new String[] { "/" };
+	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		
+		return new Filter[] { characterEncodingFilter };
 	}
 
 }
