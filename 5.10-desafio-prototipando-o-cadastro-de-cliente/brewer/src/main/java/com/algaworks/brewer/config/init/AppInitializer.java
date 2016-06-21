95419b57cc1a866fb27ffcb4d4100ddff966f398
Normandes Junior <normandesjr@gmail.com>
2016-06-21 08:45:50 -0300

    Projeto do curso

---
+++ 5.10-desafio-prototipando-o-cadastro-de-cliente/brewer/src/main/java/com/algaworks/brewer/config/init/AppInitializer.java
@@ -0,0 +1,24 @@
+package com.algaworks.brewer.config.init;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.algaworks.brewer.config.WebConfig;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] { WebConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+}
---
+++ 5.5-melhorando-a-organizacao-do-layout/brewer/src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -0,0 +1,42 @@
+package com.algaworks.brewer.model;
+
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+public class Cerveja {
+
+	@NotBlank(message = "SKU é obrigatório")
+	private String sku;
+	
+	@NotBlank(message = "Nome é obrigatório")
+	private String nome;
+	
+	@Size(min = 1, max = 50, message = "O tamanho da descrição deve estar entre 1 e 50")
+	private String descricao;
+	
+	public String getSku() {
+		return sku;
+	}
+
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}
