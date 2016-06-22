705433e739aa3d212ff444ba05d9b7b48d9997ec
Normandes Junior <normandesjr@gmail.com>
2016-06-22 11:50:40 -0300

    Projeto do curso

---
+++ 5.15-adicionando-o-favicon/brewer/src/main/java/com/algaworks/brewer/config/init/AppInitializer.java
@@ -0,0 +1,36 @@
+package com.algaworks.brewer.config.init;
+
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.algaworks.brewer.config.WebConfig;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] { WebConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		
+		return new Filter[] { characterEncodingFilter };
+	}
+
+}
