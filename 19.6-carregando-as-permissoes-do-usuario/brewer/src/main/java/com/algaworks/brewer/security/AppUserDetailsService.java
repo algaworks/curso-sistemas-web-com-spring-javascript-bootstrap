6f655f10aae84d024498077b70618dc8f43f1079
Normandes Junior <normandesjr@gmail.com>
2016-08-01 10:39:53 -0700

    Projeto do curso

---
+++ 19.6-carregando-as-permissoes-do-usuario/brewer/src/main/java/com/algaworks/brewer/security/AppUserDetailsService.java
@@ -0,0 +1,43 @@
+package com.algaworks.brewer.security;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.algaworks.brewer.model.Usuario;
+import com.algaworks.brewer.repository.Usuarios;
+
+@Service
+public class AppUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private Usuarios usuarios;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Optional<Usuario> usuarioOptional = usuarios.porEmailEAtivo(email);
+		Usuario usuario = usuarioOptional.orElseThrow(() -> new UsernameNotFoundException("Usuário e/ou senha incorretos"));
+		return new User(usuario.getEmail(), usuario.getSenha(), getPermissoes(usuario));
+	}
+
+	private Collection<? extends GrantedAuthority> getPermissoes(Usuario usuario) {
+		Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+		
+		List<String> permissoes = usuarios.permissoes(usuario);
+		permissoes.forEach(p -> authorities.add(new SimpleGrantedAuthority(p.toUpperCase())));
+		
+		return authorities;
+	}
+
+}
---
+++ 19.6-carregando-as-permissoes-do-usuario/brewer/src/main/java/com/algaworks/brewer/service/CadastroCidadeService.java
@@ -0,0 +1,30 @@
+package com.algaworks.brewer.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.algaworks.brewer.model.Cidade;
+import com.algaworks.brewer.repository.Cidades;
+import com.algaworks.brewer.service.exception.NomeCidadeJaCadastradaException;
+
+@Service
+public class CadastroCidadeService {
+
+	@Autowired
+	private Cidades cidades;
+	
+	@Transactional
+	public void salvar(Cidade cidade) {
+		Optional<Cidade> cidadeExistente = cidades.findByNomeAndEstado(cidade.getNome(), cidade.getEstado());
+		if (cidadeExistente.isPresent()) {
+			throw new NomeCidadeJaCadastradaException("Nome de cidade já cadastrado");
+		}
+		
+		cidades.save(cidade);
+	}
+
+	
+}
